82a3f4279e37dbcc0df084f71c5b8b53c90c4c84
borjaelxd <103900363+borjaelxd@users.noreply.github.com>
2022-09-10 16:28:38 -0300

    Update FormConverter.java

--- src/main/java/me/liuli/ez4h/translators/converters/FormConverter.java
+++ src/main/java/me/liuli/ez4h/translators/converters/FormConverter.java
@@ -2,7 +2,7 @@ package me.liuli.ez4h.translators.converters;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.github.steveice10.mc.protocol.packet.ingame.server.ServerPluginMessagePacket;
+import com.github.steveice10.mc.protocol.packet.ingame.serverbound.ServerPluginMessagePacket;
 import me.liuli.ez4h.minecraft.Client;
 import me.liuli.ez4h.minecraft.data.play.Form;
 import me.liuli.ez4h.utils.OtherUtil;
